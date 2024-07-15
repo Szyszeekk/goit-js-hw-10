0c2fa9315e0a1bee2244a0a07c7cdecb9228378d
Szyszeekk <dzikciekocha@gmail.com>
2024-07-15 20:23:03 +0200

    push1

---
+++ src/js/01-timer.js
@@ -0,0 +1,76 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const button = document.querySelector('.button');
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-minutes]');
+const secondsElement = document.querySelector('[data-seconds]');
+let countdownInterval;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    let userSelectedDate = selectedDates[0];
+    console.log(userSelectedDate);
+    if (userSelectedDate < new Date()) {
+      window.alert('Please choose a date in the future');
+      button.disabled = true;
+    } else {
+      button.disabled = false;
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateCountdown(targetDate) {
+  const now = new Date().getTime();
+  const difference = targetDate.getTime() - now;
+
+  const { days, hours, minutes, seconds } = convertMs(difference);
+
+  daysElement.textContent = formatTime(days);
+  hoursElement.textContent = formatTime(hours);
+  minutesElement.textContent = formatTime(minutes);
+  secondsElement.textContent = formatTime(seconds);
+}
+
+button.addEventListener('click', () => {
+  const datetimePicker = flatpickr('#datetime-picker');
+  const selectedDate = datetimePicker.selectedDates[0];
+
+  updateCountdown(selectedDate);
+
+  countdownInterval = setInterval(() => {
+    updateCountdown(selectedDate);
+  }, 1000);
+
+  button.disabled = true;
+});
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
